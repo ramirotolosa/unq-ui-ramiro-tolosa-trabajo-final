8b90de32bad0fcf57f85f676b6e9b153b8f55a1a
ramirotolosa <ramirotolosa99@gmail.com>
2026-07-11 04:56:14 -0300

    Primera implementacion de la pagina welcome. Creo componentes reutilizables Button y TextInput.

--- src/pages/Welcome/Welcome.jsx
+++ src/pages/Welcome/Welcome.jsx
@@ -1,7 +1,12 @@
+import logo from '../../assets/logo.svg';
+import PlayerNameForm from './PlayerNameForm/PlayerNameForm';
+import './Welcome.css';
+
 function Welcome() {
   return (
-    <div>
-      <h1>Welcome</h1>
+    <div className="welcome">
+      <img className="welcome__logo" src={logo} alt="Palabras Encadenadas" />
+      <PlayerNameForm />
     </div>
   );
 }
